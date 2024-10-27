3f280561714956a00cd2b32aea3dd6a708a0049e
Seyoon Ko <kos@ucla.edu>
2024-10-26 20:53:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -91,4 +91,4 @@ def parse_contents(contents, filename, date):
         None)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=60000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
